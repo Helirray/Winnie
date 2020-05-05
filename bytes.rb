a08fa0eb34f10e8932db5d3c6ec3ba6dcf92472a
rakhimov.r <rakhimov.ruslan.r@yandex.ru>
2020-05-05 16:07:19 +0600

    init

---
+++ bytes.rb
@@ -0,0 +1,16 @@
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+# Чтобы узнать, сколько байт в числе, воспользуемся методом size
+number = 42
+puts "В числе #{number} — #{number.size} байт"
+
+# Чтобы узнать, сколько байт в строке, воспользуемся методом bytesize
+string = 'Василий'
+puts "В строке \"#{string}\" — #{string.bytesize} байт"
